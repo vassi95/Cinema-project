9a876ee1308500ce27d3e63b3a1e3a61535e8929
vassi95 <vassi__95@abv.bg>
2015-05-07 21:02:43 +0300

    update

--- create.py
+++ create.py
@@ -1,10 +1,14 @@
 import sqlite3
 import sys
 
-db_name = sys.argv[1]
-sql_file = sys.argv[2]
+if sys.argc != 3:
+    print("Enter 3 arguments!")
+else:
+    db_name = sys.argv[1]
+    sql_file = sys.argv[2]
 
 conn = sqlite3.connect(db_name)
+
 with open(sql_file, "r") as f:
     conn.executescript(f.read())
     conn.commit()
