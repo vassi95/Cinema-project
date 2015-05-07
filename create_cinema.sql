b81a3efb6609ee0c875b9d5c4a8889045caf1741
Sevgin Mustafov <sevgin.mustafov@outlook.com>
2015-05-07 20:37:58 +0300

    Creating db schema

---
+++ create_cinema.sql
@@ -0,0 +1,20 @@
+DROP TABLE IF EXISTS Movies;
+
+CREATE TABLE Movies(id INTEGER UNIQUE PRIMARY KEY NOT NULL,
+                    name TEXT
+                    rating real);
+
+DROP TABLE IF EXISTS Projections;
+CREATE TABLE Projections(id INTEGER UNIQUE PRIMARY KEY NOT NULL,
+                         movie_id INTEGER,
+                         projection_type TEXT,
+                         projection_date TEXT,
+                         projection_time TEXT,
+                         FOREIGN KEY(movie_id) REFERENCES Movies(id));
+
+DROP TABLE IF EXISTS Reservations; 
+CREATE TABLE Reservations(id INTEGER UNIQUE PRIMARY KEY NOT NULL, 
+                          username TEXT,
+                          projection_id INTEGER, 
+                          row INTEGER, 
+                          col INTEGER)
