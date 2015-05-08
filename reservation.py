f16f7fc92c0aa8ff9d66928bc1297aa77b90b874
Sevgin Mustafov <sevgin.mustafov@outlook.com>
2015-05-08 19:18:56 +0300

    reservations updated

--- reservation.py
+++ reservation.py
@@ -8,11 +8,28 @@ class Reservation:
     FROM Reservations
     WHERE projection_id=? '''
 
+    MAKE_RESERVATION='''
+    INSERT INTO Reservations(id, username, projection_id, row, col)
+    VALUES(?, ?, ?, ?, ?) '''
+
+    @classmethod
+    def get_last_id(cls, conn):
+        curr = conn.cursor()
+        curr.execute('''SELECT MAX(id) FROM Reservations''')
+        return curr.fetchone()[0]
 
     @classmethod
     def free_spots(cls, conn, id):
         cursor = conn.cursor()
         cursor.execute(cls.GET_RESERVED_PLACES, (id, ))
-        result = curr.fetchone()[0]
+        result = cursor.fetchone()[0]
         total_spots = HALL_SIZE[0]*HALL_SIZE[1]
         return total_spots - result
+
+    @classmethod
+    def reserve(cls, conn, tpl):
+        user, proj_id, row, col = tpl
+        id = cls.get_last_id(conn) + 1
+        crsr=conn.cursor()
+        crsr.execute(cls.MAKE_RESERVATION, (id, user, proj_id, row, col))
+        conn.commit()
