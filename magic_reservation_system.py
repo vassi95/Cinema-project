ff0daacc49268c74787d9c427c4e4ff67bfe6597
vassi95 <vassi__95@abv.bg>
2015-05-08 20:56:06 +0300

    Add some code and update

--- magic_reservation_system.py
+++ magic_reservation_system.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 import sqlite3
 from movies import Movie
+from projections import Projection
+from reservation import Reservation
 
 conn = sqlite3.connect('cinema.db')
 
@@ -9,17 +11,14 @@ def show_movies():
     print('Current movies:')
     mov = Movie.show_movies(conn)
     for row in mov:
-        print('{[]}-{}-{()}'.format(row[0], row[1], row[2]))
+        print('[{}]-{}-({})'.format(row[0], row[1], row[2]))
 
 
 def show_movie_projections(movie_id):
     print('Projections for movie:')
-    cursor = conn.cursor()
-    result = cursor.execute("SELECT Movies.name, projection_date, projection_time \
-        FROM Movies JOIN Projections \
-        ON Movies.id=Projections.movie_id ")
-    for row in result:
-        print('{}-{}-{}'.format(row[0], row[1], row[2]))
+    proj = Projection.get_projection_from_db(conn, (movie_id))
+    for row in proj:
+        print('[{}]-{}-{}'.format(row[0], row[1], row[2]))
 
 
 def make_reservation():
@@ -36,35 +35,39 @@ def make_reservation():
     if movie_id == 'give_up':
         return
     else:
+        movie_id = int(movie_id)
         show_movie_projections(movie_id)
     projection_id = input("Step 3 (Projection): Choose a projections>")
     if projection_id == 'give_up':
         return
+    projection_id = int(projection_id)
     print('Available seats (marked with a dot):')
-    # sth that prints the available seats
-    # function which makes the seats occuiped
+    print(Reservation.free_spots(conn, 1))
+    occupied_seats = []
     chosen_seats = []
     for ticket in range(tickets):
         seat_free = False
         while not seat_free:
-            chosen = input("Step 4 (Seats): Choose seat {}>".format(ticket + 1))
+            chosen = input("Step 4 (Seats): Choose seat {}>".format(ticket+1))
             if chosen == 'give_up':
                 return
             chosen = tuple(int(x.strip()) for x in chosen.split(','))
-            # if the seat is occupied:print('This seat is already taken!')
-            # proverka za pove4e ot 10 na shirina i visochina na masiva elif chosen[0] > 10
-            # or chosen[0] < 1 or chosen[1] > 10 or chosen[1] < 1:print('Cinema is 10x10!') else:
-            seat_free = True
-            # adds to occupied .append(chosen)
-            chosen_seats.append(chosen)
+            if Reservation.check_if_is_reserved(conn, chosen) == True:
+                print('This seat is already taken!')
+            elif chosen[0] > 10 or chosen[0] < 1 or chosen[1] > 10 or chosen[1] < 1:
+                print('Cinema is 10x10!')
+            else:
+                seat_free = True
+                occupied_seats.append(chosen)
+                chosen_seats.append(chosen)
     # printing the reservation
     confirmation = input('Step 5 (Confirm - type "finalize") >')
     if confirmation == 'give_up':
         return
     if confirmation == 'finalize':
         for seat in chosen_seats:
-            # make new reservation (name, projection_id, seat[0], seat[1])
-            print('Thanks! Enjoy the movie!')
+            Reservation.reserve(conn, (name, projection_id, seat[0], seat[1]))
+        print('Thanks! Enjoy the movie!')
 
 
 def exit():
@@ -84,6 +87,8 @@ def show_help():
 
 def main():
     print(show_movies())
+    print(show_movie_projections(3))
+    print(make_reservation())
 
 if __name__ == '__main__':
     main()
--- reservation.py
+++ reservation.py
@@ -1,16 +1,20 @@
 #!/usr/bin/env python3
 from settings import HALL_SIZE
 
+
 class Reservation:
 
-    GET_RESERVED_PLACES='''
+    GET_RESERVED_PLACES = '''
     SELECT COUNT(id) as reserved
     FROM Reservations
     WHERE projection_id=? '''
 
-    MAKE_RESERVATION='''
+    MAKE_RESERVATION = '''
     INSERT INTO Reservations(id, username, projection_id, row, col)
     VALUES(?, ?, ?, ?, ?) '''
+
+    GET_RESERVED_SEATS = '''
+    SELECT COUNT(id) FROM Reservations WHERE row=? AND col=?'''
 
     @classmethod
     def get_last_id(cls, conn):
@@ -30,14 +34,24 @@ class Reservation:
     def reserve(cls, conn, tpl):
         user, proj_id, row, col = tpl
         id = cls.get_last_id(conn) + 1
-        crsr=conn.cursor()
+        crsr = conn.cursor()
         crsr.execute(cls.MAKE_RESERVATION, (id, user, proj_id, row, col))
         conn.commit()
 
-    def print_occupied(cls, conn, tpl_lst):
-        rows, cols = HALL_SIZE
-        hall = [['.' for x in range(rows)] for y in range(cols)]
-        for element in tpl_lst:
-            x, y = element
-            hall[x][y] = 'X'
+    @classmethod
+    def check_if_is_reserved(cls, conn, tpl):
+        row, col = tpl
+        crsr = conn.cursor()
+        crsr.execute(cls.GET_RESERVED_SEATS, (row, col))
+        a = crsr.fetchone()[0]
+        if a != 0:
+            return True
+        return False
+
+    # def print_occupied(cls, conn, tpl_lst):
+    #     rows, cols = HALL_SIZE
+    #     hall = [['.' for x in range(rows)] for y in range(cols)]
+    #     for element in tpl_lst:
+    #         x, y = element
+    #         hall[x][y] = 'X'
 
