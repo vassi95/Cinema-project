a6743a34c439d2d0676ca6e27e66bbe9427884ac
vassi95 <vassi__95@abv.bg>
2015-05-08 20:04:56 +0300

    Update

--- projections.py
+++ projections.py
@@ -13,13 +13,12 @@ class Projection:
     ORDER BY projection_date '''
 
     @classmethod
-    def get_projection_from_db(cls, conn, *filters):
+    def get_projection_from_db(cls, conn, m_id):
         cursor = conn.cursor()
-        if 2 == len(filters):
-            result = cursor.execute(cls.GET_PROJECTION_ON_DATE,
-                                    (filters[0], filters[1]))
-        elif 1 == len(filters):
-            result = cursor.execute(cls.GET_PROJECTION_BY_ID,
-                                    (filters[0], ))
-
+        # if 2 == len(filters):
+        #     result = cursor.execute(cls.GET_PROJECTION_ON_DATE,
+        #                             (filters[0], filters[1]))
+        # elif 1 == len(filters):
+        #
+        result = cursor.execute(cls.GET_PROJECTION_BY_ID, (m_id, ))
         return result
